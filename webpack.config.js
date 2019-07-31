83bae3744200f4934aa50851991338d557e11112
Jessica Vaughan <jessicavaughan@Jessicas-MacBook-Pro.local>
2019-07-30 17:05:32 -0700

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client.index.js',
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      }, // ends first rule
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/build',
+    proxy: { '/api': 'http://localhost:3000' },
+  }
+};
